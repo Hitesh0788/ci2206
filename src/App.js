db0f5bd371815a5863fed02474e5e192ab0f0d39
Hitesh0788 <hitesh19981013@gmail.com>
2022-08-23 03:44:25 +0530

    added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
      <h1>Today Class Was About Continuous Integration</h1>
      <h1>This is Trying Out What We Learnt In Class. </h1>
      <h1>This is Difficult To Do</h1>
+     <h1>Continuous Integration</h1>
     </div>
   );
 }
